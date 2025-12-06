76171e56db927707774223addce0640032d8d8c5
Mille Yin <mille.yin@gmail.com>
2025-12-06 16:39:40 +0800

    更新版号

--- Sources/NavigationKit/NavigationKit.swift
+++ Sources/NavigationKit/NavigationKit.swift
@@ -13,7 +13,7 @@ import NavigationCoreKit
 
 public enum NavigationKit {
     /// NavigationKit 版本号
-    public static let version: String = "1.3.0(2025030)"
+    public static let version: String = "1.4.0(2025032)"
 }
 
 //MARK: - Elevation
